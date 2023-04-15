19c2797ba7214e0e6b2ab0e9fcf770e9d823eb1b
Ritika <ritikasharma1154@gmail.com>
2023-04-16 02:44:13 +0530

    First Commit

--- ARRAY/day2_merge_overlapping_intervals.cpp
+++ ARRAY/day2_merge_overlapping_intervals.cpp
@@ -37,3 +37,34 @@ vector<Interval> Solution::merge(vector<Interval> &A) {
     }
     return v;
 }
+//BRUTE FORCE
+vector < pair < int, int >> merge(vector < pair < int, int >> & arr) {
+
+    int n = arr.size();
+    sort(arr.begin(), arr.end());
+    vector < pair < int, int >> ans;
+
+    for (int i = 0; i < n; i++) {
+        int start = arr[i].first, end = arr[i].second;
+
+        //since the intervals already lies 
+        //in the data structure present we continue
+        if (!ans.empty()) {
+            if (start <= ans.back().second) {
+                continue;
+            }
+        }
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j].first <= end) {
+                end = max(end, arr[j].second);
+            }
+        }
+    
+        end = max(end, arr[i].second);
+
+        ans.push_back({start, end});
+    }
+
+    return ans;
+}
