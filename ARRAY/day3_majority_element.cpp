4b1d372ab602e03b896d95deedd84e4daf0b8826
Ritika <ritikasharma1154@gmail.com>
2023-04-16 22:22:52 +0530

    first commit

---
+++ ARRAY/day3_majority_element.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+// USING Boyer Moore’s Voting Algorithm
+//Find the Majority Element that occurs more than N/2 times
+int majorityElement(vector<int> v) {
+
+    //size of the given array:
+    int n = v.size();
+    int cnt = 0; // count
+    int el; // Element
+
+    //applying the algorithm:
+    for (int i = 0; i < n; i++) {
+        if (cnt == 0) {
+            cnt = 1;
+            el = v[i];
+        }
+        else if (el == v[i]) cnt++;
+        else cnt--;
+    }
+
+    //checking if the stored element
+    // is the majority element:
+    int cnt1 = 0;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == el) cnt1++;
+    }
+
+    if (cnt1 > (n / 2)) return el;
+    return -1;
+}
+
+//Majority Elements(>N/3 times) | Find the elements that appears more than N/3 times in the array
+vector < int > majorityElement(int nums[], int n) {
+  int sz = n;
+  int num1 = -1, num2 = -1, count1 = 0, count2 = 0, i;
+  for (i = 0; i < sz; i++) {
+    if (nums[i] == num1)
+      count1++;
+    else if (nums[i] == num2)
+      count2++;
+    else if (count1 == 0) {
+      num1 = nums[i];
+      count1 = 1;
+    } else if (count2 == 0) {
+      num2 = nums[i];
+      count2 = 1;
+    } else {
+      count1--;
+      count2--;
+    }
+  }
+  vector < int > ans;
+  count1 = count2 = 0;
+  for (i = 0; i < sz; i++) {
+    if (nums[i] == num1)
+      count1++;
+    else if (nums[i] == num2)
+      count2++;
+  }
+  if (count1 > sz / 3)
+    ans.push_back(num1);
+  if (count2 > sz / 3)
+    ans.push_back(num2);
+  return ans;
+}
