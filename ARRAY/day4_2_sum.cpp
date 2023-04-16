b72cd6b3449669f54c34fc2df36d2d77395bd70e
Ritika <ritikasharma1154@gmail.com>
2023-04-16 22:54:05 +0530

    first commit

---
+++ ARRAY/day4_2_sum.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+//SORTING BASED SOLUTION
+string twoSum(int n, vector<int> &arr, int target) {
+    sort(arr.begin(), arr.end());
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == target) {
+            return "YES";
+        }
+        else if (sum < target) left++;
+        else right--;
+    }
+    return "NO";
+}
+
+//HASHING BASED SOLUTION
+vector<int> twoSum(int n, vector<int> &arr, int target) {
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        int num = arr[i];
+        int moreNeeded = target - num;
+        if (mpp.find(moreNeeded) != mpp.end()) {
+            return {mpp[moreNeeded], i};
+        }
+        mpp[num] = i;
+    }
+    return { -1, -1};
+}
